1bcc2bb822ece323df475705ecfdb8a4104aa377
DanielGord0n <gordondan2@gmail.com>
2021-11-10 15:25:12 -0500

    StartedGameBoard

--- src/main/java/com/battleshipics4u/ics4u_cpt_battleship/HelloApplication.java
+++ src/main/java/com/battleshipics4u/ics4u_cpt_battleship/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Testing");
+        stage.setTitle("BattleShip");
         stage.setScene(scene);
         stage.show();
         // Testing this
