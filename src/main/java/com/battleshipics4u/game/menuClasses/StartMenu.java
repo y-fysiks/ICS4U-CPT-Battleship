03049a77437ca92d2047e80aa0d0b0c99bb3cd19
Yubo Wang <yubo0809@outlook.com>
2021-12-25 17:51:03 -0500

    drag and drop for carrier testing

--- src/main/java/com/battleshipics4u/game/menuClasses/StartMenu.java
+++ src/main/java/com/battleshipics4u/game/menuClasses/StartMenu.java
@@ -1,40 +1,11 @@
 package com.battleshipics4u.game.menuClasses;
 
 import java.io.IOException;
-import java.io.InputStream;
-import javafx.scene.Group;
-import javafx.scene.Scene;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class StartMenu extends Menu {
     
     public StartMenu(Stage stage) throws IOException {
         super("start-menu.fxml");
-//        ClassLoader classLoader = getClass().getClassLoader();
-//        InputStream inputStream = classLoader.getResourceAsStream("battleship_logo.png");
-        Pane pane = new HBox(15);
-
-        Image logo = new Image("battleship_logo.png");
-//        ImageView selectedImage = new ImageView(logo);
-        
-        pane.getChildren().add(new ImageView(logo));
-//        
-//        selectedImage.setImage(logo);
-//        selectedImage.setX(10);
-//        selectedImage.setY(10);
-//        selectedImage.setFitWidth(575);
-//        selectedImage.setPreserveRatio(true);
-//        
-//        Group root = new Group(selectedImage);
-        Scene scene = new Scene(pane, 600, 300);
-        stage.setScene(scene);
-        stage.show();
-
-        //logoStream = new FileInputStream("battleship_logo.png");
-        //logo = new Image(logoStream);
     }
 }
--- src/main/java/com/battleshipics4u/game/menuControllers/PlayerBoardSetController.java
+++ src/main/java/com/battleshipics4u/game/menuControllers/PlayerBoardSetController.java
@@ -1,8 +1,26 @@
 package com.battleshipics4u.game.menuControllers;
 
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 
 public class PlayerBoardSetController {
 
-    public static ImageView carrierImgView;
+    public ImageView carrierImgView;
+    public ImageView battleshipImgView;
+    public ImageView destroyerImgView;
+    public ImageView cruiserImgView;
+    public ImageView submarineImgView;
+    boolean moveCarrier = false;
+
+    public void mouseClickedCarrier(MouseEvent mouseEvent) {
+        moveCarrier = !moveCarrier;
+    }
+
+    public void move(MouseEvent mouseEvent) {
+        if (moveCarrier) {
+            carrierImgView.setX(mouseEvent.getX() - 36);
+            carrierImgView.setY(mouseEvent.getY() - 30);
+        }
+
+    }
 }
